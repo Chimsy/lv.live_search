83990a0283dcaf8bc46c7ff67b1ed9655f3b3062
Golden Chimusinde <golden.chimusinde@econet.co.zw>
2020-08-20 17:31:46 +0200

    Moved Assets to FROM CDN

--- resources/views/full_text_search.blade.php
+++ resources/views/full_text_search.blade.php
@@ -3,10 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
     <title>Full Text Search in Laravel 6 using Ajax</title>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"/>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+    <script src="{{asset('js/jquery.min.js')}}"></script>
+    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}"/>
+    <script src="{{asset('js/bootstrap.min.js')}}"></script>
 </head>
 <body>
 <div class="container">
